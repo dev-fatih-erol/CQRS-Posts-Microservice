4900fc01ec18273030a50ac93cede89fdbe0c2c2
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-06-12 22:58:00 +0300

    .

--- Posts.Api/Controllers/PostController.cs
+++ Posts.Api/Controllers/PostController.cs
@@ -1,20 +1,31 @@
 ﻿using System.Threading.Tasks;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Posts.Application.Queries;
 
 namespace Posts.Api.Controllers
 {
     public class PostController : Controller
     {
-        public PostController()
+        private readonly IMediator _mediator;
+
+        public PostController(IMediator mediator)
         {
-                
+            _mediator = mediator;
         }
 
         [HttpGet]
         [Route("Post/{id:length(24)}")]
         public async Task<IActionResult> GetById([FromRoute]string id)
         {
-            return Ok();
+            var post = await _mediator.Send(new GetPostByIdQuery(id));
+
+            if (post != null)
+            {
+                return Ok(post);
+            }
+
+            return NotFound();
         }
     }
 }
