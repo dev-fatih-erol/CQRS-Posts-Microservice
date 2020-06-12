db14deb94641d19b68f80905e1fa4e98e3e6f71f
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-06-12 22:52:58 +0300

    .

--- Posts.Api/Controllers/PostController.cs
+++ Posts.Api/Controllers/PostController.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Posts.Api.Controllers
 {
     public class PostController : Controller
     {
+        public PostController()
+        {
+                
+        }
+
+        [HttpGet]
+        [Route("Post/{id:length(24)}")]
+        public async Task<IActionResult> GetById([FromRoute]string id)
+        {
+            return Ok();
+        }
     }
 }
