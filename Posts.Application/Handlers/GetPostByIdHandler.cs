68676c290283707f97231a54a64344a8cbe11a7b
Fatih Erol <50841052+dev-fatih-erol@users.noreply.github.com>
2020-06-25 23:33:02 +0300

    .

---
+++ Posts.Application/Handlers/GetPostByIdHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Posts.Application.Queries;
+using Posts.Infrastructure.Entities;
+using Posts.Infrastructure.Services;
+
+namespace Posts.Application.Handlers
+{
+    public class GetPostByIdHandler : IRequestHandler<GetPostByIdQuery, Post>
+    {
+        private readonly IPostService _postService;
+
+        public GetPostByIdHandler(IPostService postService)
+        {
+            _postService = postService;
+        }
+
+        public async Task<Post> Handle(GetPostByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _postService.GetById(request.Id);
+        }
+    }
+}
